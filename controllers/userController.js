ae5599379aac2c5d5c347724c984f16297800478
Adam Dubey <adubey0311@gmail.com>
2021-08-17 17:56:20 -0400

    [AD][08M1721] Wire up GraphQL server and client app

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID);
+
+exports.findOrCreateUser = async token => {
+    // verify auth token
+    const googleUser = await verifyAuthToken(token);
+    // check if user exists?
+    const user = await checkIfUserExists(googleUser.email)
+    // if user exists, return; otherwise create new user
+    return user ? user : createNewUser(googleUser);
+};
+
+
+const verifyAuthToken = async token => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.OAUTH_CLIENT_ID
+        })
+        return ticket.getPayload();
+    } catch (err) {
+        console.error("Error verifying auth token", err);
+    }
+};
+
+const checkIfUserExists = async email => await User.findOne({ email }).exec();
+
+const createNewUser = googleUser => {
+    const { name, email, picture } = googleUser;
+    const user = { name, email, picture };
+    return new User(user).save();
+};
---
+++ resolvers.js
@@ -0,0 +1,21 @@
+const { AuthenticationError } = require("apollo-server");
+
+const user = {
+    _id: "1",
+    name: "Adam",
+    email: "dev@test.com",
+    picture: "https://cloudinary.com/abcx"
+};
+
+const authenticated = next => (root, args, ctx, info) => {
+    if (!ctx.currentUser) {
+        throw new AuthenticationError('You must be logged in!')
+    }
+    return next(root, args, ctx, info)
+}
+
+module.exports = {
+    Query: {
+        me: authenticated((root, args, ctx) => ctx.currentUser)
+    }
+};

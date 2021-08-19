7fdef6cf2b96a555f9d44121ee6e7ca133316dd7
Adam Dubey <adubey0311@gmail.com>
2021-08-19 16:57:57 -0400

    [AD][08M1921] Expanding Map component functionality

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import PinIcon from "./PinIcon";
+
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
 
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewPort] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPosition] = useState(null);
+  useEffect(() => {
+    getUserPosition()
+  }, []);
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+    
+        setViewPort({ ...viewport, latitude, longitude });
+        setUserPosition({ latitude, longitude });
+      });
+    };
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiYWRuaXgiLCJhIjoiY2tzamRybm44MmN0aTJwcXRlYmtzMnV2biJ9.KgJJMgRKXr-AYoX6Hm3iDA"
+        onViewportChange={viewport => setViewPort(viewport)}
+        {...viewport}
+      >
+      <div className={classes.navigationControl}>
+        <NavigationControl
+          onViewportChange={viewport => setViewPort(viewport)}
+         />
+
+      </div>
+      {userPosition && (
+        <Marker
+          latitude={userPosition.latitude}
+          longitude={userPosition.longitude}
+          offsetLeft={-19}
+          offsetTop={-37}
+        >
+          <PinIcon size={40} color="orange" />
+        </Marker>
+      )}
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
